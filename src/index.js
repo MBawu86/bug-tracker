5f00d7a05d3708916ca0067edfa8bc1ac2118359
Molly Behrens <algorithm.warrior.unlocked@gmail.com>
2021-02-03 10:27:16 -0600

    sets up redux state

--- src/index.js
+++ src/index.js
@@ -1,11 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import {Provider} from 'react-redux'
+import {configureStore, combineReducers} from '@redux js/toolkit'
 import App from './App';
 
+//reducers
+import authReducer from ',/Controllers/Redux/authSlice'
+import bugReducer from './Controllers/Redux/bugSlicer'
+import userReducer from './Controllers/Redux/userSlice'
+
+//organize and keep seperate in state
+//combine reducer
+//redux configure
+const reducer = combineReducers({
+authReducer: authReducer,
+bug: bugReducer,
+user: userReducer
+})
+
+//configure store
+
+const store = configureReducer({
+  reducer 
+})
 
 ReactDOM.render(
-    <App />,
+  <Provider store = {store}>
+    <App />
+  </Provider>,
+
   document.getElementById('root')
 );
 
